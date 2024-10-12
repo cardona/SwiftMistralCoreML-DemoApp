3cece8b55ef914a28fb3c4df08908b21f5fa552d
Oscar Cardona <ocardona@gmail.com>
2024-10-12 18:34:04 +0200

    Initial Commit

---
+++ SwiftMistralCoreML-DemoApp/SwiftMistralCoreML_DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMistralCoreML_DemoAppApp.swift
+//  SwiftMistralCoreML-DemoApp
+//
+//  Created by Oscar on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMistralCoreML_DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftMistralCoreML-DemoAppTests/SwiftMistralCoreML_DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMistralCoreML_DemoAppTests.swift
+//  SwiftMistralCoreML-DemoAppTests
+//
+//  Created by Oscar on 12/10/24.
+//
+
+import Testing
+@testable import SwiftMistralCoreML_DemoApp
+
+struct SwiftMistralCoreML_DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
